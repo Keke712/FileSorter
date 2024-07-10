d0ff3ed213104500ec5bf520662d63e2f28417b3
Henriqué Pieterhons <henriquepieterhons@gmail.com>
2024-07-11 00:56:58 +0200

    Update main.py
    Added progressbar
    Added options

--- main.py
+++ main.py
@@ -1,65 +1,101 @@
 
-import os
+import os, sys
 import shutil
 import time
 from datetime import datetime
 import win32file, win32con, pywintypes
+from tqdm import tqdm
+
+choice = input("Sort by:\n1. Creation date\n2. Modification date (recommended)\n: ")
+choice2 = input("\nSave old files (Y/N): ")
 
 directory = os.getcwd()
-# Directory where files are copied and sorted in folders
-save_to = directory+"\\Sorted"
 
-# Check if the directory to sort images is created
+# Directory where files are copied and sorted in folders
+rollback_folder = directory+"\\Rollback"
+
+# Check if dir is created else: create it
 def check_dir(dr):
     if not os.path.exists(dr):
         os.mkdir(dr)
 
-check_dir(save_to)
-
 def main():
-    for root, dirs, files in os.walk(directory):
-        for name in files:
-            # For all files ending by..
-            if name.lower().endswith(('.png', '.jpg', '.jpeg', '.mp4')):
-                created_time = os.path.getctime(name)
-                modified_time = os.path.getmtime(name)
+    total_files = sum(len(files) for _, _, files in os.walk(directory))
 
-                # Format file date
-                month_year = datetime.fromtimestamp(created_time).strftime('%Y %B')
-                path_to_save = save_to+"\\"+month_year
+    with tqdm(total=total_files, desc="Progression") as pbar:
+        for root, dirs, files in os.walk(directory):
+            for name in files:
+                # For all files ending by..
+                if name.lower().endswith(('.png', '.jpg', '.jpeg', '.mp4')):
+                    try:
+                        created_time = os.path.getctime(name)
+                    except Exception:
+                        print("\nFolders cannot be moved")
+                        sys.exit()
 
+                    modified_time = os.path.getmtime(name)
 
-                check_dir(path_to_save)
-                file_to_save = path_to_save+"\\"+name
+                    # Format file date
+                    # Choice with the first input
+                    if choice == "1":
+                        month_year = datetime.fromtimestamp(created_time).strftime('%Y %B')
+                    elif choice == "2":
+                        month_year = datetime.fromtimestamp(modified_time).strftime('%Y %B')
+                    else:
+                        print("\nSort option is not valid")
+                        sys.exit()
+                    
+                    path_to_save = directory+"\\"+month_year
+                    
+                    check_dir(path_to_save)
+                    file_to_save = path_to_save+"\\"+name
 
-                if not os.path.exists(file_to_save):
-                    # Copy file with metadata
-                    shutil.copy2(name, file_to_save)
+                    if not os.path.exists(file_to_save):
+                        # Copy file with metadata
+                        shutil.copy2(name, file_to_save)
 
-                    # Initialize Win32 objects
-                    creation_time_obj = pywintypes.Time(created_time)
-                    modified_time_obj = pywintypes.Time(modified_time)
+                        # Initialize Win32 objects
+                        creation_time_obj = pywintypes.Time(created_time)
+                        modified_time_obj = pywintypes.Time(modified_time)
 
-                    # Get a handle to the copied file (open)
-                    handle = win32file.CreateFile(
-                        file_to_save,
-                        win32con.GENERIC_WRITE,
-                        0,
-                        None,
-                        win32con.OPEN_EXISTING,
-                        0,
-                        None
-                    )
+                        # Get a handle to the copied file (open)
+                        handle = win32file.CreateFile(
+                            file_to_save,
+                            win32con.GENERIC_WRITE,
+                            0,
+                            None,
+                            win32con.OPEN_EXISTING,
+                            0,
+                            None
+                        )
 
-                    # Set the creation time of the file to the modification time of the source file
-                    win32file.SetFileTime(
-                        handle,
-                        CreationTime=creation_time_obj,
-                        LastWriteTime=modified_time_obj
-                    )
+                        # Set the creation time of the file to the modification time of the source file
+                        win32file.SetFileTime(
+                            handle,
+                            # /!\ Modify the code here if you need
+                            # Setting the creation time on modified time for precision
+                            # Example: Copying on photos from phone to your PC will replace the creationTime with the current day
 
-                    # Close the file handle
-                    win32file.CloseHandle(handle)
+                            CreationTime=modified_time_obj,
+                            LastWriteTime=modified_time_obj
+                        )
+
+                        # Close the file handle
+                        win32file.CloseHandle(handle)
+
+                        if choice2 == "Y":
+                            # Y: Move old file in rollback folder
+                            check_dir(rollback_folder)
+                            shutil.move(name, rollback_folder)
+                        elif choice2 == "N":
+                            # N: Delete files
+                            os.remove(name)
+                        else:
+                            print("\nSave old file option is not valid")
+                            sys.exit()
+
+                        # Progression bar updated
+                        pbar.update(1)
 
 if __name__ == "__main__":
     main()
